8ec8a56bb313a8ce6e5e4729b83099ff71f38692
Nikolay Igotti <Nikolay.Igotti@huawei.com>
2024-06-27 18:31:18 +0300

    Missing files

---
+++ peer_lib/external/common/src/index.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export { int8, uint8, int32, uint32, int64, uint64, float32, float64, CustomTextDecoder, CustomTextEncoder, isFunction, functionOverValue, className, lcClassName, asFloat64 } from "@koalaui/compat"
+export { clamp, lerp, modulo, parseNumber, isFiniteNumber, getDistancePx } from "./math"
+export { hashCodeFromString } from "./strings"
+export { KoalaProfiler } from "./KoalaProfiler"
+export * from "./PerfProbe"
+export * from "./Errors"
+export * from "./Events"
+export * from "./Finalization"
+export * from "./Matrix33"
+export * from "./Matrix44"
+export * from "./Point3"
+export * from "./Point"
+export { SHA1Hash, createSha1 } from "./sha1"
+export { UniqueId } from "./uniqueId"
+export * from "./koalaKey"
