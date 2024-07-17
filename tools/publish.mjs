0fa360f6a331b32ef2dfc2f071c5ec8c60d3c683
Korobeinikov Evgeny <korobeinikov.evgeny@huawei.com>
2024-07-17 07:48:52 +0000

    !419 Publish idlize package to GZ * publish to gitlab

--- tools/publish.mjs
+++ tools/publish.mjs
@@ -7,21 +7,51 @@ import { execSync } from "child_process"
 const CWD = process.cwd()
 const prebuiltPath = path.join(CWD, ".packages")
 
+const keyIdlizeRegistry = "@azanat:registry"
+const keyKoalaRegistry = "@koalaui:registry"
+const koalaRegistry = "https://rnd-gitlab-msc.huawei.com/api/v4/projects/3921/packages/npm/"
+const idlizeRegistry = "https://nexus.bz-openlab.ru:10443/repository/koala-npm/"
+
+function setRegistry(key, value) {
+    execSync(`npm config set ${key} ${value}`)
+}
+
+function getRegistry(key) {
+    execSync(`npm config get ${key}`)
+}
+
 function pack() {
     if (fs.existsSync(prebuiltPath))
         fs.rmSync(prebuiltPath, { recursive: true })
     fs.mkdirSync(prebuiltPath)
-    execSync(`npm pack --pack-destination ${ prebuiltPath }`)
+    execSync(`npm pack --pack-destination ${prebuiltPath}`)
+}
+
+function publishToOpenlab() {
+
+    pack()
+    setRegistry(keyIdlizeRegistry, idlizeRegistry)
+
+    let packageName = fs.readdirSync(prebuiltPath)[0]
+    console.log(chalk.green(`> Publishing ${packageName}...`))
+    execSync(`npm publish ${path.join(prebuiltPath, packageName)}`)
+
+}
+
+function publishToGitlab() {
+
+    pack()
+    setRegistry(keyIdlizeRegistry, koalaRegistry)
+
+    let packageName = fs.readdirSync(prebuiltPath)[0]
+    console.log(chalk.green(`> Publishing ${packageName}...`))
+    execSync(`npm publish ${path.join(prebuiltPath, packageName)}`)
+
+    setRegistry(keyIdlizeRegistry, idlizeRegistry)
 }
 
 function publish() {
-    pack()
-
-    let packageName = fs.readdirSync(prebuiltPath)[0]
-
-    console.log(chalk.green(`> Publishing ${ packageName }...`))
-    execSync(`npm publish ${ path.join(prebuiltPath, packageName) } --registry https://nexus.bz-openlab.ru:10443/repository/koala-npm/${packageName}`)
+    process.env.KOALA_BZ == true ? publishToOpenlab() : publishToGitlab()
 }
-
 
 publish()
