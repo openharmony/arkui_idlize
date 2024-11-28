7dd1a20547abf9202022f99586d3b2064bdd4518
Anton Tarasov <tarasov.anton1@huawei.com>
2024-11-28 08:15:08 +0000

    !1071 Fix vsync decl in ApiImpl * Fix vsync decl in ApiImpl

--- templates/impl_prologue.cc
+++ templates/impl_prologue.cc
@@ -45,8 +45,7 @@ namespace ApiImpl {
     void SetRangeUpdater(Ark_NodeHandle nodePtr, Ark_Int32 updaterId);
     void SetLazyItemIndexer(Ark_VMContext vmContext, Ark_NodeHandle nodePtr, Ark_Int32 indexerId);
     Ark_PipelineContext GetPipelineContext(Ark_NodeHandle node);
-    void SetVsyncCallback(Ark_VMContext vmContext, Ark_PipelineContext pipelineContext, Ark_Int32 callbackId);
-    void UnblockVsyncWait(Ark_VMContext vmContext, Ark_PipelineContext pipelineContext);
+    void SetVsyncCallback(Ark_PipelineContext pipelineContext, Ark_VsyncCallback callback);
     void SetChildTotalCount(Ark_NodeHandle node, Ark_Int32 totalCount);
     void ShowCrash(Ark_CharPtr message);
 } // namespace OHOS::Ace::NG::ApiImpl
