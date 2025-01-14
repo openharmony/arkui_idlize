dfa09ed60b1758f7f5ff4b5ec905619cc0e7127e
vopl <vopl@bk.ru>
2025-01-14 14:27:49 +0000

    !1205 improve dowload-sdk * fix repo directory * force fetch for remote-tracked refs * separated setup * improve download-sdk

--- download-sdk.mjs
+++ download-sdk.mjs
@@ -15,16 +15,84 @@
 
 import * as fs from "node:fs"
 import { execSync } from "node:child_process"
+import path from "node:path"
+import { fileURLToPath } from 'url'
 
-const dir = "./interface_sdk-js"
+const __thisScript = fileURLToPath(import.meta.url)
+const __setupFile = path.format({ ...path.parse(__thisScript), base: '', ext: '.json' })
+const setup = JSON.parse(fs.readFileSync(__setupFile))
+console.log("use setup:", setup)
+const repoUrl = setup.url
+const repoRef = setup.ref
+const repoDir = `${path.dirname(__thisScript)}/interface_sdk-js`
+const gitDir = `${repoDir}/.git`
 
-if (fs.existsSync(dir)) {
-    //execSync(`cd ${dir} && git pull`)
-} else {
-    console.log("Downloading sdk")
-    execSync("git clone --depth=1 https://gitee.com/openharmony/interface_sdk-js.git")
+// utilities
+const git = `git --git-dir=${gitDir} --work-tree=${repoDir}`
+let repoRefKind
+if (repoRef.match(/^[0-9a-zA-Z]{40}$/)) repoRefKind = 'hash'
+else if (repoRef.startsWith("origin/")) repoRefKind = 'remote'
+else repoRefKind = 'local'
+
+// initial clone
+if (!fs.existsSync(repoDir) || !fs.existsSync(gitDir)) {
+  console.log("no git directories exists, try to clone repo")
+  exec(`git --git-dir=${gitDir} clone ${repoUrl} ${repoDir}`)
 }
 
+// unshallow
+if (exec(`${git} rev-parse --is-shallow-repository`).stdout.trim() == "true") {
+  console.log("git shallow state found, try to fetch full history")
+  exec(`${git} fetch --prune --unshallow`)
+}
+
+// cleanup
+let res = exec(`${git} status --short`)
+if (res.stdout) {
+  console.log(res.stdout)
+  console.log("git status reports changes, try to clean")
+  exec(`${git} checkout -f`)
+  exec(`${git} clean -xfd`)
+}
+
+// fetch
+let needFetch = false
+if (repoRefKind === 'hash') {
+  const exists = exec(`${git} cat-file -e ${repoRef}`, false).code == 0
+  needFetch = !exists
+} else
+  needFetch = true
+
+if (needFetch) {
+  exec(`${git} fetch --prune`)
+}
+
+// checkout
+const currentRefs = exec(`${git} log -n1 --pretty="format:%H, %D" --decorate=short`).stdout
+  .split(/,\s*/)
+  .filter(e => !e.includes("HEAD"))
+  .filter(e => !!e)
+
+if (repoRefKind === 'local') {
+  const currentBranch = exec(`${git} branch --show-current`).stdout.trim()
+  if (currentBranch)
+    currentRefs.push(currentBranch)
+}
+
+if (!currentRefs.includes(repoRef)) {
+  console.log("current references:", currentRefs)
+  console.log(`try to checkout ref: ${repoRef}`)
+  exec(`${git} checkout ${repoRef}`)
+}
+
+// follow remote
+if (repoRefKind === 'local') {
+  console.log(`try to forward local branch: ${repoRef}`)
+  exec(`${git} merge --ff-only`)
+}
+// repo ready
+
+// link to project
 let sdk = "./sdk"
 let components = "./interface_sdk-js/api/\@internal/component/ets"
 if (!fs.existsSync(sdk)) {
@@ -35,4 +103,30 @@ if (!fs.existsSync(sdk)) {
       console.log("Symlink failed, try to copy")
       fs.cpSync(components, sdk + "/component", { recursive: true })
     }
+}
+
+// all done
+console.log("sdk ready")
+process.exit(0)
+
+// utilities
+function exec(cmd, dieIfFailed=true) {
+  const result = {
+    code: 0,
+    stdout: "",
+    stderr: ""
+  }
+  try {
+    console.log(`exec [${cmd}]`)
+    result.stdout = execSync(cmd, {stdio: 'pipe'}).toString()
+  } catch (e) {
+    result.code = e.status
+    result.stdout = e.stdout.toString()
+    result.stderr = e.stderr.toString()
+    if (dieIfFailed) {
+      console.log("fail:", result)
+      process.exit(result.code || 1)
+    }
+  }
+  return result
 }
