9ebb46a84945338e626eab2bcbec39102a566596
scherbatiyalexander <scherbatiy.alexander@huawei-partners.com>
2025-02-28 06:57:59 +0000

    !1679 Check NativeBuffer.read/write() in unit tests for ArkTS * Move external to NativeBuffer.readByte()/write() methods * Check NativeBuffer.read/write() in unit tests for ArkTS

--- ohosgen/tests/unit/src/panda/app_panda.ts
+++ ohosgen/tests/unit/src/panda/app_panda.ts
@@ -10,9 +10,14 @@ export function runPanda() {
   const buffer = rlt.outData;
   console.log(buffer)
   console.log(`buffer length: ${buffer.length}`)
-  // TBD: add NativeBuffer.readByte(...) method
-  // for (let i = 0; i < buffer.length; i++) {
-  //   console.log(`buffer elem[${i}]: ${buffer.readByte(i)}`)
-  // }
+  for (let i = 0; i < buffer.length; i++) {
+    console.log(`read value by index ${i} from the buffer`)
+    console.log(`buffer elem[${i}]: ${buffer.readByte(i)}`)
+    const value = 100 + i
+    console.log(`write value ${value} to the buffer with index ${i}`)
+    buffer.writeByte(i, 100 + i)
+    console.log(`buffer elem[${i}]: ${buffer.readByte(i)}`)
+    console.log()
+  }
 }
 
