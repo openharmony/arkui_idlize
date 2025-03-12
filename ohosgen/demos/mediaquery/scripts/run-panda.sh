41982e3d103281b5e9fd3811423bc001036fa503
scherbatiyalexander <scherbatiy.alexander@huawei-partners.com>
2025-03-12 07:31:56 +0000

    !1810 Set the USE_PERF env variable to use perf for mediaquery benchmarks

--- ohosgen/demos/mediaquery/scripts/run-panda.sh
+++ ohosgen/demos/mediaquery/scripts/run-panda.sh
@@ -8,4 +8,12 @@ out_dir=build/panda
 
 bootfiles=$external_dir/incremental/runtime/build/incremental.abc:$external_dir/interop/build/interop.abc
 
-LD_LIBRARY_PATH=$PWD/$out_dir perf record -g --call-graph dwarf,8192 --event cycles:Pu --aio --sample-cpu $arkts_dir/ark $out_dir/app.abc --ark-boot-files $bootfiles --ark-entry-point @mediaquery.src.panda.main.ETSGLOBAL::main
+if [-v USE_PERF]; then
+    PERF_PARAMS="perf record -g --call-graph dwarf,8192 --event cycles:Pu --aio --sample-cpu"
+    echo "PERF_PARAMS=$PERF_PARAMS"
+else
+    echo "USE_PERF is not set."
+    echo "Set the USE_PERF env variable to use perf 'export USE_PERF=true'."
+fi
+
+LD_LIBRARY_PATH=$PWD/$out_dir $PERF_PARAMS $arkts_dir/ark $out_dir/app.abc --ark-boot-files $bootfiles --ark-entry-point @mediaquery.src.panda.main.ETSGLOBAL::main
