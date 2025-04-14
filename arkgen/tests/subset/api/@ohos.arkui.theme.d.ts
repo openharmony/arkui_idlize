81406c612370fdbf9120be0f9f8b193bdaba88ea
Peter Z <byte.fisher@huawei-partners.com>
2025-04-14 07:04:24 +0000

    !1988 Fixed CustomObjects * Predefined SystemBarStyle * Predefined ImageModifier, SymbolGlyphModifier, UIContext * Predefined Context, ComponentContent, CustomTheme, Scene, RestrictedWorker

--- arkgen/tests/subset/api/@ohos.arkui.theme.d.ts
+++
@@ -1,5 +0,0 @@
-export declare interface Theme {}
-export declare interface Colors {}
-export declare interface CustomTheme {}
-export declare type CustomColors = Partial<Colors>;
-export declare class ThemeControl {}
