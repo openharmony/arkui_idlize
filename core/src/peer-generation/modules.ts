d0d99294a8d544942b1d7c2c0fb61a5e760211fe
scherbatiyalexander <scherbatiy.alexander@huawei-partners.com>
2025-07-24 08:09:56 +0000

    !2422 Check applicable modules in isInCurrentModule(...) function

--- core/src/peer-generation/modules.ts
+++ core/src/peer-generation/modules.ts
@@ -1,6 +1,9 @@
 import { generatorConfiguration, ModuleConfiguration } from "../config"
 import * as idl from "../idl"
 
+
+const modulesCache = new Map<string, ModuleConfiguration>()
+
 export function isInModule(node: idl.IDLNode, module: ModuleConfiguration): boolean
 export function isInModule(packageName: string, module: ModuleConfiguration): boolean
 export function isInModule(nodeOrPackage: idl.IDLNode | string, module: ModuleConfiguration): boolean {
@@ -19,14 +22,28 @@ export function getModuleFor(nodeOrPackage: idl.IDLNode | string): ModuleConfigu
     if (typeof nodeOrPackage === "object")
         return getModuleFor(idl.getPackageName(nodeOrPackage))
     const packageName = nodeOrPackage
+    let module = modulesCache.get(packageName)
+    if (module) return module
+    module = getApplicableModuleFor(packageName)
+    modulesCache.set(packageName, module)
+    return module
+}
+
+function getApplicableModuleFor(packageName: string): ModuleConfiguration {
     const config = generatorConfiguration()
     const applicableModules = [...config.modules.values()].filter(module => isInModule(packageName, module))
     if (applicableModules.length === 0) {
-        if (nodeOrPackage === '') {
+        if (packageName === '') {
             console.error("WARNING: use current module for empty package")
             return currentModule()
         }
-        throw new Error(`Package ${packageName} is not listed in any module`)
+        if (packageName.startsWith(`idlize.`)) {
+            return currentModule()
+        }
+        const modules = [...config.modules.keys()].map(it => `"${it}"`).join(", ")
+        throw new Error(`Package "${packageName}" is not listed in any module.`
+            + ` Add the "${packageName}" to the existed list of modules [${modules}] or new one in the configuration file`)
+
     }
     if (applicableModules.length > 1)
         throw new Error(`Package ${packageName} listed in ${applicableModules.length} packages: ${applicableModules.map(it => it.name).join(", ")}`)
@@ -44,8 +61,9 @@ export function currentModule(): ModuleConfiguration {
 export function isInCurrentModule(node: idl.IDLNode): boolean
 export function isInCurrentModule(packageName: string): boolean
 export function isInCurrentModule(nodeOrPackage: idl.IDLNode | string): boolean {
-    if (typeof nodeOrPackage === 'string')
-        return isInModule(nodeOrPackage, currentModule())
-    else
-        return isInModule(nodeOrPackage, currentModule())
+    // check the nodeOrPackage belongs to some module
+    const module = typeof nodeOrPackage === 'string'
+        ? getModuleFor(nodeOrPackage)
+        : getModuleFor(nodeOrPackage)
+    return generatorConfiguration().moduleName == module.name
 }
