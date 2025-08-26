7416d6c1f9b5592e4c66adc7122952307bbac0fe
platonovvladislav <platonov.vladislav@h-partners.com>
2025-08-26 15:12:27 +0000

    !2545 Check materialized by FQN

--- core/src/peer-generation/isMaterialized.ts
+++ core/src/peer-generation/isMaterialized.ts
@@ -26,7 +26,7 @@ export function isMaterialized(declaration: idl.IDLInterface, resolver: Referenc
         return false
     }
 
-    if (generatorConfiguration().forceMaterialized.includes(declaration.name)) {
+    if (generatorConfiguration().forceMaterialized.some(r => r === idl.getFQName(declaration))) {
         return true
     }
 
