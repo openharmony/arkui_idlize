611fa9cdc389abca5a9d78141e4f8bd631948704
pavelmironchik <pavel.mironchik@huawei.com>
2025-10-24 10:30:01 +0000

    !2753 complete ohosgen system tests for gn
    Signed-off-by: Pavel Mironchik <pavel.mironchik@huawei.com>
    Signed-off-by: kolegovilya <kolegov.ilya@huawei.com>

--- ohosgen/tests/system/build-panda.sh
+++ ohosgen/tests/system/build-panda.sh
@@ -2,10 +2,11 @@
 set -e
 shopt -s globstar # to make **/*.abc recursive
 
-external_dir=../../../external
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+external_dir=$SCRIPT_DIR/../../../external
 arkts_dir=$external_dir/incremental/tools/panda/arkts
-out_dir=$1/build/panda
+out_dir=$SCRIPT_DIR/$1/build/panda
 
 mkdir -p $out_dir/out
-npx fast-arktsc --compiler $arkts_dir/ui2abc --link-name $out_dir/app.abc --config ./$1/arktsconfig.json --simultaneous
+npx fast-arktsc --compiler $arkts_dir/ui2abc --link-name $out_dir/app.abc --config $SCRIPT_DIR/$1/arktsconfig.json --simultaneous
 ninja -C $out_dir/out
