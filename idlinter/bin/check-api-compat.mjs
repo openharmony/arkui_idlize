e7b00324a55862f5a88d272ec75e35e4d78e4645
Peter Z <byte.fisher@huawei-partners.com>
2025-11-20 15:33:40 +0000

    !2826 Documentation on idlinter API compat checker script
    Signed-off-by: Pavel Mironchik <pavel.mironchik@huawei.com>
    Signed-off-by: kolegovilya <kolegov.ilya@huawei.com>

--- idlinter/bin/check-api-compat.mjs
+++ idlinter/bin/check-api-compat.mjs
@@ -3,49 +3,56 @@ import path from "node:path"
 import { execSync } from "node:child_process"
 
 const tmpdir = `/tmp/api-compat-${process.pid}`
-let [repo_dir, commit, base] = process.argv.slice(2)
+let isIdlize = false;
+const args = process.argv.slice(2);
+if (args[0] === '--idlize') {
+    isIdlize = true;
+    args.shift();
+}
+let [repo_dir, commit, base] = args;
 if (!commit || !(fs.existsSync(repo_dir) && fs.lstatSync(repo_dir).isDirectory())) {
     console.log('Usage:')
-    console.log('        $(basename $0) <repo> <commit> [<base>]')
+    console.log('        $(basename $0) [--idlize] <repo> <commit> [<base>]')
     console.log('Check <commit> for compatibility against <base>.')
     console.log('If <base> is missing, check against previous commit (<base> is <commit>~).')
     process.exit(-1)
 }
-if (!base)
-    base = execSync(`git rev-parse ${commit}~`, {stdio: 'pipe'}).toString().slice(0, 8)
 
 repo_dir = path.resolve(repo_dir)
 process.chdir(`${path.dirname(process.argv[1])}/../..`)
 
-// ====================================================================================================
-// The following is an adapter. Because our CI runs on one workspace (idlize), but checks another
-// one (interface_sdk-js), we hereby translate idlize commit hashes into corresponding hashes in
-// the SDK workspace. This is done by examining the tools/download-sdk.json file.
-//
-// In normal operation, when CI runs on SDK workspace itself, sdkBase == base, and sdkCommit == commit.
-function getSdkCommit(commit) {
-    exec(`git fetch origin ${commit}`)
-    const downloadSdkJson = execSync(`git show origin/${commit}:tools/download-sdk.json`).toString()
-    return JSON.parse(downloadSdkJson).ref.slice(0, 8)
+if (isIdlize) {
+    // The following is an adapter. Because our CI runs on one workspace (idlize), but checks another
+    // one (interface_sdk-js), we hereby translate idlize commit hashes into corresponding hashes in
+    // the SDK workspace. This is done by examining the tools/download-sdk.json file.
+    //
+    // In normal operation, when CI runs on SDK workspace itself, just use `base` and `commit` as is.
+    function getSdkCommit(commit) {
+        exec(`git fetch origin ${commit}`)
+        const downloadSdkJson = execSync(`git show origin/${commit}:tools/download-sdk.json`).toString()
+        return JSON.parse(downloadSdkJson).ref.slice(0, 8)
+    }
+    base = getSdkCommit(base)
+    commit = getSdkCommit(commit)
+    console.log(`Checking ${commit} against ${base}`)
+    if (base === commit) {
+        console.log('SDK versions are the same, skipping compatibility check')
+        process.exit(0)
+    }
+} else {
+    if (!base)
+        base = execSync(`git -C ${repo_dir} rev-parse ${commit}~`, {stdio: 'pipe'}).toString().slice(0, 8)
 }
-const sdkBase = getSdkCommit(base)
-const sdkCommit = getSdkCommit(commit)
-console.log(`Checking ${sdkCommit} against ${sdkBase}`)
-if (sdkBase === sdkCommit) {
-    console.log('SDK versions are the same, skipping compatibility check')
-    process.exit(0)
-}
-// ====================================================================================================
 
-// exec('npm run compile -C runner')
-// exec('npm run compile -C idlinter')
+exec('npm run compile -C runner')
+exec('npm run compile -C idlinter')
 
-commit2idl(sdkBase)
-commit2idl(sdkCommit)
+commit2idl(base)
+commit2idl(commit)
 
 console.clear()
 try {
-    exec(`node idlinter compat ${tmpdir}.${sdkBase}.idl ${tmpdir}.${sdkCommit}.idl`)
+    exec(`node idlinter compat ${tmpdir}.${base}.idl ${tmpdir}.${commit}.idl`)
 } catch (error) {
     // errors are output to the console, so just exit with a non-zero code
     process.exit(1)
