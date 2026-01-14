d32f373747ea9f31c961989bec96a148448e2bc0
kolegovilya <kolegov.ilya@huawei.com>
2026-01-14 13:17:30 +0300

    Add kit package
    Signed-off-by: kolegovilya <kolegov.ilya@huawei.com>

--- tools/utils.mjs
+++ tools/utils.mjs
@@ -77,6 +77,7 @@ export const all_packages = [
     new Package(path.join(IDLIZE_HOME, "arktscgen")),
     new Package(path.join(IDLIZE_HOME, "core")),
     new Package(path.join(IDLIZE_HOME, "ost")),
+    new Package(path.join(IDLIZE_HOME, "kit")),
     new Package(path.join(IDLIZE_HOME, "libohos")),
     new Package(path.join(IDLIZE_HOME, "linter")),
     new Package(path.join(IDLIZE_HOME, "idlinter")),
