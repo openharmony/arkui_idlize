8c829e0ef08175b4debea803be4bebabd3aaf570
yalixie <yali.xie@huawei-partners.com>
2026-02-20 16:58:29 +0300

    Update scrapper
    Signed-off-by: yalixie <yali.xie@huawei-partners.com>

--- scraper/src/main.ts
+++ scraper/src/main.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /*
  * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +15,6 @@
  * limitations under the License.
  */
 
-import { scraper } from "./app";
+import { scraper } from "./app.js";
 
 scraper(process.argv.slice(2))
