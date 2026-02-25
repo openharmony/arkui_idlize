2578baffda3b17493528e09313b9672ed4fe9123
Pavel Mironchik <pavel.mironchik@huawei.com>
2026-02-25 18:50:31 +0300

    support Opt_Opt_
    Signed-off-by: Pavel Mironchik <pavel.mironchik@huawei.com>

--- libohos/src/peer-generation/printers/StructPrinter.ts
+++ libohos/src/peer-generation/printers/StructPrinter.ts
@@ -95,7 +95,6 @@ export class StructPrinter {
         const forwardDeclarations = this.library.createLanguageWriter(Language.CPP)
         const concreteDeclarations = this.library.createLanguageWriter(Language.CPP)
         const seenNames = new Set<string>()
-        seenNames.clear()
         const noDeclaration = ["Int32", "Tag", 'number', 'boolean', 'String', 'void']
         const declTargets = collectDeclarationTargets(this.library, true)
         for (const target of declTargets) {
@@ -112,13 +111,15 @@ export class StructPrinter {
             if (!nameAssigned) {
                 throw new Error(`No assigned name for an ${idl.IDLKind[target.kind!]}`)
             }
-            if (seenNames.has(nameAssigned)) {
-                continue
+            if (!idl.isOptionalType(target)) {
+                // optional types handled inside printOptionalIfNeeded
+                if (seenNames.has(nameAssigned)) {
+                    continue
+                }
+                seenNames.add(nameAssigned)
             }
-            seenNames.add(nameAssigned)
             if (idl.isPrimitiveType(target, 'Object')) {
                 // Object type is already defined as interop resource
-                this.printOptionalIfNeeded(forwardDeclarations, concreteDeclarations, writeToString, idl.createPrimitiveType('Object'), seenNames)
                 continue
             }
             if (idl.isInterface(target) && generatorConfiguration().forceResource.includes(target.name)) {
@@ -133,7 +134,8 @@ export class StructPrinter {
             let isAccessor = idl.isInterface(target) && isMaterialized(target, this.library)
             let noBasicDecl = isAccessor || noDeclaration.includes(nameAssigned)
             if (idl.isOptionalType(target)) {
-                continue
+                this.printOptionalIfNeeded(forwardDeclarations, concreteDeclarations, writeToString, target.type, seenNames)
+                continue;
             } else if (idl.isEnum(target) || idl.isEnumMember(target)) {
                 const enumTarget = idl.isEnumMember(target) ? target.parent : target
                 const stringEnum = isStringEnum(enumTarget)
@@ -252,12 +254,9 @@ export class StructPrinter {
         const nameOptional = idl.isType(target)
             ? concreteDeclarations.getNodeName(idl.createOptionalType(target))
             : generatorConfiguration().OptionalPrefix + cleanPrefix(concreteDeclarations.getNodeName(target as idl.IDLEntry), generatorTypePrefix())
-        if (forceOptional || nameOptional.includes("Opt_CustomObject")) {
-            if (seenNames.has(nameOptional)) {
-                return
-            }
+        if (seenNames.has(nameOptional)) {
+            return
         }
-
         seenNames.add(nameOptional)
 
         if (nameAssigned !== "Optional" && nameAssigned !== "RelativeIndexable") {
