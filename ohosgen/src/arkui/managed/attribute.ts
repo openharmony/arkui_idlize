78ce1014c8b9e3517fbd38491a3d91861642ef67
Peter <byte.fisher@huawei-partners.com>
2026-03-02 12:05:54 +0300

    Modifier generation
    Signed-off-by: Peter <byte.fisher@huawei-partners.com>

--- ohosgen/src/arkui/managed/attribute.ts
+++ ohosgen/src/arkui/managed/attribute.ts
@@ -14,12 +14,16 @@
  */
 
 import * as idl from "@idlizer/core/idl"
-import { T, Builders, managedName, Ts, createProducer, expectType, OhosSeed, E, FunctionDeclaration, OhosProducerContext, Hs, LWType, Md, expectExpr } from "@idlizer/libohos"
+import { isDefined, isRoot, capitalize } from "@idlizer/core"
+import { T, Ts, E, S, Hs, LWType, LWExpression, LWKind, Builders, FunctionDeclaration,
+  ClassDeclaration, managedName, createProducer, expectExpr, expectType,
+  OhosSeed, OhosProducerContext
+} from "@idlizer/libohos"
 import { ArkUIRole } from ".."
-import { isDefined } from "@idlizer/core"
 
 function isComponentAttribute(node: idl.IDLInterface) {
-  return idl.hasExtAttribute(node, idl.IDLExtendedAttributes.Component)
+  return isRoot(node.name) ||
+    idl.hasExtAttribute(node, idl.IDLExtendedAttributes.Component)
 }
 
 function isCommonMethodProperty(prop: idl.IDLProperty) {
@@ -33,7 +37,9 @@ function isAttributeModifier(prop: idl.IDLProperty) {
 function superClassForRole(node: idl.IDLInterface, role: 'peer' | 'component', ctx: OhosProducerContext): LWType | undefined {
   return node.name === 'CommonMethod'
     ? T.c(role === 'peer' ? 'PeerNode' : 'ComponentBase')
-    : expectType(ctx, node.inheritance[0], role)
+    : node.inheritance.length
+      ? expectType(ctx, node.inheritance[0], role)
+      : undefined
 }
 
 export const attributeProducer = createProducer(
@@ -48,7 +54,8 @@ export const attributeProducer = createProducer(
               ? expectType(ctx, node.inheritance[0], 'managed')
               : undefined)
           .$(),
-        createImpl(ctx, node, attrName)
+        createImpl(ctx, node, attrName),
+        createModifier(ctx, node, attrName)
       ].filter(isDefined),
       trigger: node.properties
         .filter(it => !isAttributeModifier(it))
@@ -68,7 +75,7 @@ export const peerProducer = createProducer<idl.IDLInterface, ArkUIRole<idl.IDLIn
         idl.createParameter('peerPtr', idl.createPrimitiveType('i32')),
         idl.createParameter('id', idl.createPrimitiveType('i32')),
       ], undefined)
-    ctor.parent = node
+    ctor.parent = node ///parent should in fact be peer, not attribute
     const nativeModuleCall = expectExpr(ctx, ctor, 'native-module')
     return {
       continuation: T.c(peerName),
@@ -89,7 +96,8 @@ export const peerProducer = createProducer<idl.IDLInterface, ArkUIRole<idl.IDLIn
             .block()
               .decl('peerId').value().call('nextId').receiver('PeerNode').$().$().$()
               .decl('peerPtr').value().call(nativeModuleCall).arg('peerId').arg('flags').$().$().$()
-              .decl('peer').value().ctor(peerName).arg('peerPtr').arg('peerId').arg(E.c(`'${componentName}'`)).arg('flags').$().$().$()
+              .decl('peer').value().ctor(peerName)
+                .arg('peerPtr').arg('peerId').arg(E.c(`'${componentName}'`)).arg('flags').$().$().$()
               .call('setPeer').receiver('component').arg('peer').$()
               .return().value('peer').$().$().$()
           .$()
@@ -150,4 +158,200 @@ function createImpl(ctx: OhosProducerContext, attrNode: idl.IDLInterface, attrNa
     .block()
       .decl('receiver').value(rememberCall).$()
       .call(nodeAttachCall).$().$().$()
+}
+
+function modifierFieldName(propName: string, index: number): string {
+  return `_${propName}_${index}_value`
+}
+
+function createModifier(ctx: OhosProducerContext, attrNode: idl.IDLInterface, attrName: string): ClassDeclaration {
+  const baseName = attrName.replace(/Attribute$/, '')
+  const modifierName = baseName + 'Modifier'
+  const parentModifierName = attrNode.inheritance.length
+    ? managedName((attrNode.inheritance[0] as idl.IDLReferenceType).name.replace(/(Attribute)?$/, 'Modifier'))
+    : modifierName // fallback, e.g. CommonMethod extends itself
+  const peerName = baseName + 'Peer'
+
+  // Separate regular properties from attributeModifier
+  const regularProps = attrNode.properties.filter(it => !isAttributeModifier(it))
+  const attrModProps = attrNode.properties.filter(it => isAttributeModifier(it))
+
+  // Resolve types for regular properties
+  const propTypes = regularProps.map(prop => expectType(ctx, prop.type, 'managed'))
+
+  // Build fields with initializers
+  const fields: ClassDeclaration['fields'] = [
+    { name: '_instanceId', type: Ts.prim.number, expression: E.c(-1) },
+    { name: '_state', type: T.c('ModifierState'), expression: E.instance('ModifierState', []) },
+    { name: '_addr', type: T.c('ArrayBuffer'), expression: E.instance('ArrayBuffer', [E.c(4096)]) },
+    { name: '_flagArray', type: T.c('Uint8Array'), expression: E.instance('Uint8Array', [E.get(E.v('this'), '_addr')]) },
+  ]
+
+  // Add per-property value fields
+  regularProps.forEach((prop, i) => {
+    fields.push({
+      name: modifierFieldName(prop.name, i),
+      type: Ts.optional(propTypes[i]),
+    })
+  })
+
+  // isUpdater: () => boolean = () => false
+  fields.push({
+    name: 'isUpdater',
+    type: T.fn([], Ts.prim.boolean),
+    expression: E.lambda([], S.e(E.c('false'))),
+  })
+
+  // Build the class
+  const classBuilder = Builders.class(modifierName)
+    .extends(T.c(parentModifierName))
+    .implements(T.c(attrName))
+    .implements(T.c('AttributeModifier', T.c(attrName)))
+    .fields(fields)
+
+  // Constructor: super() + fill flagArray with 0
+  classBuilder.ctor()
+    .block()
+      .call('super').$()
+      .call('fill').receiver().access('_flagArray').receiver('this').$().$().arg(E.c(0)).$()
+      .$().$()
+
+  // setInstanceId method
+  classBuilder.method('setInstanceId')
+    .param('instanceId').type(Ts.prim.number).$()
+    .returns(Ts.prim.void)
+    .block()
+      .binary('=').left(E.get(E.v('this'), '_instanceId')).right(E.v('instanceId')).$()
+      .$().$()
+
+  // applyNormalAttribute, applyPressedAttribute, applyFocusedAttribute, applyDisabledAttribute, applySelectedAttribute
+  for (const methodName of ['applyNormalAttribute', 'applyPressedAttribute', 'applyFocusedAttribute', 'applyDisabledAttribute', 'applySelectedAttribute']) {
+    classBuilder.method(methodName)
+      .param('instance').type(T.c(attrName)).$()
+      .returns(Ts.prim.void)
+      .block().$().$()
+  }
+
+  // applyModifierPatch method
+  classBuilder.method('applyModifierPatch')
+    .param('node').typeStr('PeerNode').$()
+    .returns(Ts.prim.void)
+    .block()
+      .call('applyModifierPatch').receiver('super').arg(E.v('node')).$()
+      .call('addRef').receiver().access('_state').receiver('this').$().$().$()
+      .statements([
+        S.declaration('peer', T.c('Ark' + peerName.split('.').pop()!), false, E.cast(E.v('node'), T.c('Ark' + peerName.split('.').pop()!))),
+        S.declaration('flagArray', T.c('Uint8Array'), false, E.get(E.v('this'), '_flagArray')),
+        ...regularProps.map((prop, i) =>
+          S.if(
+            E.bin('!=', E.get(E.v('flagArray'), E.c(i)), E.c(0)),
+            S.block([
+              {
+                kind: LWKind.SwitchStatement,
+                selector: E.get(E.v('flagArray'), E.c(i)),
+                cases: [
+                  {
+                    value: E.c(1),
+                    body: [
+                      S.e(E.call(E.get(E.v('peer'), 'set' + capitalize(prop.name) + 'Attribute'), [E.get(E.v('this'), modifierFieldName(prop.name, i))])),
+                      S.e(E.bin('=', E.get(E.v('flagArray'), E.c(i)), E.c(2))),
+                      S.e(E.v('break')),
+                    ]
+                  },
+                  {
+                    value: E.c(3),
+                    body: [
+                      S.e(E.bin('=', E.get(E.v('flagArray'), E.c(i)), E.c(2))),
+                      S.e(E.v('break')),
+                    ]
+                  }
+                ],
+                default: [
+                  S.e(E.bin('=', E.get(E.v('flagArray'), E.c(i)), E.c(0))),
+                  S.e(E.call(E.get(E.v('peer'), 'set' + capitalize(prop.name) + 'Attribute'), [E.v('undefined')])),
+                  S.e(E.v('break')),
+                ]
+              } as any
+            ])
+          )
+        )
+      ])
+      .$().$()
+
+  // mergeModifier method
+  classBuilder.method('mergeModifier')
+    .param('modifier').type(T.c(modifierName)).$()
+    .returns(Ts.prim.void)
+    .block()
+      .call('mergeModifier').receiver('super').arg(E.v('modifier')).$()
+      .binary('=').left(E.get(E.v('this'), '_state')).right(E.get(E.v('modifier'), '_state')).$()
+      .statements([
+        S.declaration('flagArray', T.c('Uint8Array'), false, E.get(E.v('modifier'), '_flagArray')),
+        ...regularProps.map((prop, i) =>
+          S.if(
+            E.bin('!=', E.get(E.v('flagArray'), E.c(i)), E.c(0)),
+            S.block([
+              {
+                kind: LWKind.SwitchStatement,
+                selector: E.get(E.v('flagArray'), E.c(i)),
+                cases: [
+                  {
+                    value: E.c(1),
+                    body: [
+                      S.e(E.call(E.get(E.v('this'), prop.name), [E.get(E.v('modifier'), modifierFieldName(prop.name, i))])),
+                      S.e(E.v('break')),
+                    ]
+                  },
+                  {
+                    value: E.c(3),
+                    body: [
+                      S.e(E.call(E.get(E.v('this'), prop.name), [E.get(E.v('modifier'), modifierFieldName(prop.name, i))])),
+                      S.e(E.v('break')),
+                    ]
+                  }
+                ],
+                default: [
+                  S.e(E.call(E.get(E.v('this'), prop.name), [E.v('undefined')])),
+                  S.e(E.v('break')),
+                ]
+              } as any
+            ])
+          )
+        )
+      ])
+      .$().$()
+
+  // Per-property setter methods
+  regularProps.forEach((prop, i) => {
+    classBuilder.method(prop.name)
+      .param('value').type(propTypes[i]).$()
+      .returns(Ts.prim.self)
+      .block()
+        .if()
+          .condition(E.bin('==', E.get(E.get(E.v('this'), '_flagArray'), E.c(i)), E.c(0)))
+          .then().block()
+            .binary('=').left(E.get(E.get(E.v('this'), '_flagArray'), E.c(i))).right(E.c(1)).$()
+            .binary('=').left(E.get(E.v('this'), modifierFieldName(prop.name, i))).right(E.v('value')).$()
+            .call('fireChange').receiver().access('_state').receiver('this').$().$().$()
+            .$().$()
+          .else().block()
+            .binary('=').left(E.get(E.get(E.v('this'), '_flagArray'), E.c(i))).right(E.c(3)).$()
+            .$().$()
+          .$()
+        .return().value('this').$()
+        .$().$()
+  })
+
+  // attributeModifier stub methods
+  for (const prop of attrModProps) {
+    const propType = expectType(ctx, prop.type, 'managed')
+    classBuilder.method(prop.name)
+      .param('value').type(propType).$()
+      .returns(Ts.prim.self)
+      .block()
+        .statements([S.e(E.v('throw new Error("Not implemented")'))])
+        .$().$()
+  }
+
+  return classBuilder.$()
 }
