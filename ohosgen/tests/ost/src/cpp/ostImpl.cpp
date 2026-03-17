f6c19da52b03e47a406e27d32cf24605009c0940
yalixie <yali.xie@huawei-partners.com>
2026-03-17 11:22:34 +0300

    Add ost unit test
    Signed-off-by: yalixie <yali.xie@huawei-partners.com>

---
+++ ohosgen/tests/ost/src/cpp/ostImpl.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "unit_ost.h"
+
+#include <stdio.h>
+
+// Enum
+
+OH_UNIT_OST_OSTIntEnum ost_enums_checkOSTIntEnumImpl(OH_UNIT_OST_OSTIntEnum enumValue, OH_Int32 value)
+{
+    if (OSTINT_ENUM_E1 != 1)
+        INTEROP_FATAL("Enum OSTINT_ENUM_E1 %d does not equal to: %d", OSTINT_ENUM_E1, -1);
+
+    if (enumValue != OSTINT_ENUM_E1)
+        INTEROP_FATAL("Enum param value1 %d does not equal OSTINT_ENUM_E1: %d", enumValue, OSTINT_ENUM_E1);
+
+    return OSTINT_ENUM_E3;
+}
+
+// Sequence
+/*
+OH_Int32 ost_sequences_checkOSTSequence(const OH_UNIT_OST_Array_Int32* value)
+{
+    OH_Int32 length = value->length;
+    if (length == 0) return 0;
+
+    OH_Int32 firstValue = value->array[0];
+    OH_Int32 lastValue = value->array[length - 1];
+    if (firstValue != 3)
+        INTEROP_FATAL("The first sequence value %d does not equal to 3", firstValue);
+    if (lastValue != -7)
+        INTEROP_FATAL("The last sequence value %d does not equal to -7", firstValue);
+    return length;
+}
+*/
---
+++ ohosgen/tests/ost/src/node/compat.ts
@@ -0,0 +1,128 @@
+import { callCallback, InteropNativeModule, registerNativeModuleLibraryName, loadInteropNativeModule, checkEvents, wrapSystemApiHandlerCallback } from "@koalaui/interop"
+
+export {
+    OSTIntEnum,
+    checkOSTIntEnum,
+} from '../../generated/ts'
+
+export {
+    checkOSTSequence,
+} from '../../generated/ts'
+
+export {
+    getOSTPromise,
+} from '../../generated/ts'
+
+export type OHBuffer = ArrayBuffer
+export type OHAny = any
+
+export function init() {
+    loadInteropNativeModule()
+    InteropNativeModule._SetCallbackDispatcher(callCallback)
+    wrapSystemApiHandlerCallback()
+}
+
+export function runEventLoop() {
+    let finished = false
+    let pull = () => {
+        checkEvents()
+        if (!finished)
+            setTimeout(pull, 0)
+    };
+    setTimeout(pull, 0);
+    setTimeout(() => {
+        finished = true
+    }, 2000);
+}
+
+class UnitTestError extends Error {}
+
+export function toBigInt(value: number): bigint {
+    return BigInt(value)
+}
+
+export function checkEQ(value1: unknown, value2: unknown, comment?: string): void {
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+        const arr1 = value1 as object[]
+        const arr2 = value2 as object[]
+        if (arr1.length != arr2.length) {
+            throw new UnitTestError(`Arrays size differ: ${comment}`)
+        }
+        for(let i = 0; i < arr1.length; i++) {
+            checkEQ(arr1[i], arr2[i], comment)
+        }
+        return;
+    }
+    if (typeof value1 == "bigint" || typeof value2 == "bigint") {
+        if (value1 != value2) {
+            console.log(`value ${value1} type ${typeof value1} does not equal to the value ${value2} type ${typeof value2}`)
+            throw new UnitTestError(comment)
+        }
+        return;
+    }
+
+    if (value1 !== value2) {
+        console.log(`value ${value1} type ${typeof value1} does not equal to the value ${value2} type ${typeof value2}`)
+        throw new UnitTestError(comment)
+    }
+}
+
+export function assertDoubleEQ(value1: number, value2: number, absError: number = 0.001, comment?: string): void {
+    if (Math.abs(value2 - value1) > absError) {
+        console.log(`value ${value1} does not equal to the value ${value2} with absError: ${[absError]}`)
+        throw new UnitTestError(comment)
+    }
+}
+
+export function checkNotEQ(value1: unknown, value2: unknown, comment?: string): void {
+    if (value1 === value2) {
+        throw new UnitTestError(comment)
+    }
+}
+
+class Test {
+    constructor(
+        public readonly name: string,
+        public readonly test: () => Promise<void> | void
+    ) {}
+}
+
+export class UnitTestsuite {
+
+    private tests: Test[] = []
+    constructor(public name: string) {
+    }
+
+    addTest(testName: string, test: () => void): void {
+        this.tests.push(new Test(testName, test))
+    }
+
+    addAsyncTest(testName: string, test: () => Promise<void>): void {
+        this.tests.push(new Test(testName, test))
+    }
+
+    async run() {
+        const failedTests: string[] = []
+        for (const t of this.tests) {
+            try {
+                await t.test()
+                console.log('[ \x1b[32m%s\x1b[0m ] %s', 'PASSED', t.name);
+            } catch (ex) {
+                if (ex instanceof UnitTestError) {
+                    failedTests.push(t.name)
+                    console.log('[ \x1b[31m%s\x1b[0m ] %s', 'FAILED', t.name);
+                    console.error('...', ex.message)
+                } else {
+                    throw ex
+                }
+            }
+        }
+        if (failedTests.length) {
+            for (const name of failedTests) {
+                console.error('FAILED =>', name)
+            }
+            throw new Error("Tests failed!")
+        }
+    }
+}
+
