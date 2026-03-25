da09f0498033938865f5d71932657dd2021b132a
platonovvladislav <platonov.vladislav@h-partners.com>
2026-03-25 15:18:54 +0300

    Add license headers
    Signed-off-by: platonovvladislav <platonov.vladislav@h-partners.com>

--- codecheck_fixer/tests/fixtures/expected/cpp/comments_doc.cpp
+++ codecheck_fixer/tests/fixtures/expected/cpp/comments_doc.cpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
  /  / File with various comment styles and documentation to verify comment formatting preservation
 #include < iostream > #include < string > / *  *  * @brief Class demonstrating different comment styles and documentation formats *  * This class contains methods with various types of comments and documentation that can be very long and should be properly formatted by the formatter * / class DocumentedClass {
 public: /  /  / Default constructor with triple - slash Doxygen style comment
