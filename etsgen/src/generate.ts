67ae9c4bbf0ad94d576ab69dd2a01db155edca1f
Nazarov Konstantin <nazarov.konstantin1@huawei.com>
2026-04-16 00:28:17 +0800

    fix ETSGLOBAL usage in idlizer
    Signed-off-by: Nazarov Konstantin <nazarov.konstantin1@huawei.com>

--- etsgen/src/generate.ts
+++ etsgen/src/generate.ts
@@ -159,7 +159,7 @@ export function generateFromSts({ inputFiles, baseDir, outDir, etsConfigPath, co
         '--simultaneous'
     ]).peer
     if (!arkts.global.configIsInitialized()) throw new Error(`Wrong config: path=${etsConfigPath}`);
-    arkts.arktsGlobal.compilerContext = arkts.Context.createContextGenerateAbcForExternalSourceFiles(inputFiles)
+    arkts.arktsGlobal.compilerContext = arkts.Context.createContextSimultaneousMode(inputFiles)
     const options = arkts.Options.createOptions(new arkts.Config(arkts.global.config));
     arkts.global.arktsconfig = options.getArkTsConfig();
 
@@ -598,7 +598,7 @@ class IDLVisitor extends arkts.AbstractVisitor {
             if (arkts.isFunctionDeclaration(node)) {
                 return this.processNode(this.visitFunctionDeclaration, node)
             }
-            if (arkts.isETSModule(node) && node.ident?.name !== 'ETSGLOBAL') {
+            if (arkts.isETSModule(node) && node.isNamespace) {
                 return this.processNode(this.visitETSModule, node)
             }
             if (arkts.isVariableDeclaration(node)) {
