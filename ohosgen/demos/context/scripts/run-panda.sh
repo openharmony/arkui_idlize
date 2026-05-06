c05dbf96f3db34168a4e63fe93b28a83cee072f5
Konstantin Chukharev <chukharev.konstantin@huawei-partners.com>
2026-05-06 16:02:41 +0300

    Use absolute paths
    Signed-off-by: Konstantin Chukharev <chukharev.konstantin@huawei-partners.com>

--- ohosgen/demos/context/scripts/run-panda.sh
+++ ohosgen/demos/context/scripts/run-panda.sh
@@ -3,5 +3,5 @@ set -e
 
 node_modules_dir=../../../node_modules
 
-LD_LIBRARY_PATH=$node_modules_dir/@koalaui/interop/build:./build/panda:$LD_LIBRARY_PATH \
+LD_LIBRARY_PATH=$(realpath $node_modules_dir/@koalaui/interop/build):$(realpath ./build/panda)${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH} \
     npx smart-arkts run --project launcher
