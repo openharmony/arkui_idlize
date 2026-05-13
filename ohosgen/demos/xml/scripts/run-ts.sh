7a5306cb7e5f36a8eb574545fdb67ab73a9f995a
yalixie <yali.xie@huawei-partners.com>
2026-05-13 11:48:18 +0300

    Add NODE_PATH to run-ts.sh
    Signed-off-by: yalixie <yali.xie@huawei-partners.com>

--- ohosgen/demos/xml/scripts/run-ts.sh
+++ ohosgen/demos/xml/scripts/run-ts.sh
@@ -4,4 +4,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 node_modules_dir=../../../node_modules
 
 LD_LIBRARY_PATH=build/node:$node_modules_dir/@koalaui/interop/build:$LD_LIBRARY_PATH \
+    NODE_PATH=build/node \
     node build/node/index.js
