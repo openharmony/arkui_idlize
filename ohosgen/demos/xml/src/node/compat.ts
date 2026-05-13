1e529a80cb487e1ed9475a49f857e8080bee7952
yalixie <yali.xie@huawei-partners.com>
2026-05-13 17:15:19 +0300

    Update xmlImpl
    Signed-off-by: yalixie <yali.xie@huawei-partners.com>

--- ohosgen/demos/xml/src/node/compat.ts
+++ ohosgen/demos/xml/src/node/compat.ts
@@ -14,6 +14,7 @@ export function init() {
     loadInteropNativeModule()
     InteropNativeModule._SetCallbackDispatcher(callCallback)
     wrapSystemApiHandlerCallback()
+    registerOhosXmlApiHandler()
 }
 
 export function encodeText(text:string): ArrayBuffer {
