cc38020ad17b014e12d4672ab6fdf9c18aef40a3
platonovvladislav <platonov.vladislav@h-partners.com>
2026-06-10 16:14:33 +0300

    Move templates and demos to tests directory
    Signed-off-by: platonovvladislav <platonov.vladislav@h-partners.com>

---
+++ ohosgen/tests/demos/mediaquery/scripts/run-ts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+node_modules_dir=../../../../node_modules
+
+LD_LIBRARY_PATH=build/node:$node_modules_dir/@koalaui/interop/build:$LD_LIBRARY_PATH \
+    node build/node/index.js
