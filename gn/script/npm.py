b71fbb1233ee0fd05602e8db8da208d1ac18e9ae
Pavel Mironchik <pavel.mironchik@huawei.com>
2026-08-03 19:40:23 +0300

    move interop cpp sources to out folder
    Signed-off-by: Pavel Mironchik <pavel.mironchik@huawei.com>

--- gn/script/npm.py
+++ gn/script/npm.py
@@ -14,6 +14,8 @@
 # limitations under the License.
 
 import argparse
+import glob
+import json
 import shutil
 import subprocess
 import os
@@ -30,7 +32,11 @@ parser.add_argument("--stdlib-path", help="stdlib path")
 parser.add_argument("--target-out-path", help="out directory of built target")
 parser.add_argument("--built-file-path", help="result of building")
 parser.add_argument("--install", action="store_true", help="request npm install")
+parser.add_argument("--install-override", action="append", default=[],
+                    help="package name:version override (repeatable)")
 parser.add_argument("--install-path", help="path to install in")
+parser.add_argument("--install-depfile", help="depfile path to write after install")
+parser.add_argument("--install-depfile-target", help="output target for depfile")
 parser.add_argument("--run-tasks", nargs='+', help="npm run tasks")
 parser.add_argument("--panda-sdk-path", help="panda sdk path")
 
@@ -57,24 +63,97 @@ if args.panda_sdk_path:
 # Note: os.environ["PANDA_SDK_PATH"] = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../ui2abc/build/sdk")
 
 def run(args_list, dir = None):
-    os.chdir(dir or project_path)
+    cwd = os.getcwd()
+    try:
+        os.chdir(dir or project_path)
 
-    if os.environ.get("KOALA_LOG_STDOUT"):
-        subprocess.run(["npm"] + args_list, env=os.environ, text=True, check=True, stderr=subprocess.STDOUT)
-        return
-    result = subprocess.run(["npm"] + args_list, capture_output=True, env=os.environ, text=True)
-    with open(koala_log, "w+") as f:
-        f.write(f"npm args: {args_list}; project: {project_path}:\n" + result.stdout)
-        if result.returncode != 0:
-            f.write(f"npm args: {args_list}; project: {project_path}:\n" + result.stderr)
+        if os.environ.get("KOALA_LOG_STDOUT"):
+            subprocess.run(["npm"] + args_list, env=os.environ, text=True, check=True, stderr=subprocess.STDOUT)
+            return
+        result = subprocess.run(["npm"] + args_list, capture_output=True, env=os.environ, text=True)
+        with open(koala_log, "w+") as f:
+            f.write(f"npm args: {args_list}; project: {project_path}:\n" + result.stdout)
+            if result.returncode != 0:
+                f.write(f"npm args: {args_list}; project: {project_path}:\n" + result.stderr)
+                f.close()
+                with open(koala_log, "r") as log_file:
+                    print(log_file.read())
+                raise Exception("npm failed")
             f.close()
-            with open(koala_log, "r") as log_file:
-                print(log_file.read())
-            raise Exception("npm failed")
-        f.close()
+    finally:
+        os.chdir(cwd)
+
+def clean_workspace(root):
+    for nm in glob.glob(os.path.join(root, '**', 'node_modules'), recursive=True):
+        if os.path.isdir(nm):
+            shutil.rmtree(nm, ignore_errors=True)
+    for pl in glob.glob(os.path.join(root, '**', 'package-lock.json'), recursive=True):
+        if os.path.isfile(pl):
+            os.remove(pl)
+
+
+def collect_package_jsons(install_dir):
+    pkgs = [os.path.join(install_dir, 'package.json')]
+    root_pkg_path = pkgs[0]
+    if os.path.exists(root_pkg_path):
+        with open(root_pkg_path, 'r') as f:
+            root = json.load(f)
+        workspaces = root.get('workspaces')
+        if workspaces:
+            patterns = workspaces if isinstance(workspaces, list) else workspaces.get('packages', [])
+            for pattern in patterns:
+                ws_glob = os.path.join(install_dir, pattern, 'package.json')
+                for match in glob.glob(ws_glob, recursive=True):
+                    if os.path.isfile(match) and match not in pkgs:
+                        pkgs.append(match)
+    return sorted(pkgs)
+
+
+def write_depfile(depfile_path, target, deps):
+    with open(depfile_path, 'w') as f:
+        f.write(f'{target}:')
+        for dep in deps:
+            f.write(f' {dep}')
+        f.write('\n')
+
+
+def apply_overrides(install_dir, overrides):
+    package_json = os.path.join(install_dir, 'package.json')
+    backup_json = package_json + '.backup'
+
+    shutil.copy2(package_json, backup_json)
+
+    with open(package_json, 'r') as f:
+        pkg = json.load(f)
+
+    overrides_dict = {}
+    for ov in overrides:
+        name, version = ov.split(':', 1)
+        overrides_dict[name] = version
+    pkg['overrides'] = overrides_dict
+
+    with open(package_json, 'w') as f:
+        json.dump(pkg, f, indent=2)
+        f.write('\n')
+
+    return backup_json
+
+
+def restore_package_json(backup_path):
+    shutil.move(backup_path, backup_path.replace('.backup', ''))
+
 
 def install(dir = None):
     run(["install", "--registry", NPM_REPO, "--verbose"], dir or project_path)
+
+
+def install_with_overrides(install_dir, overrides):
+    clean_workspace(project_path)
+    backup = apply_overrides(install_dir, overrides)
+    try:
+        install(install_dir)
+    finally:
+        restore_package_json(backup)
 
 def copy_target():
     if not os.path.exists(args.built_file_path):
@@ -84,7 +163,14 @@ def copy_target():
 
 def main():
     if args.install:
-        install(args.install_path)
+        install_dir = args.install_path or project_path
+        if args.install_override:
+            install_with_overrides(install_dir, args.install_override)
+        else:
+            install(install_dir)
+        if args.install_depfile:
+            pkgs = collect_package_jsons(install_dir)
+            write_depfile(args.install_depfile, args.install_depfile_target, pkgs)
     if args.run_tasks:
         for task in args.run_tasks:
             run(["run", task])
